dea24824775a39e005f05204966faf004ea0050e
Krystian Wiewior <krystian.wiewior@gmail.com>
2020-12-12 21:37:38 +0100

    Main Activity Test Connection Made

--- app/src/main/java/com/example/gitstage/MainActivity.kt
+++ app/src/main/java/com/example/gitstage/MainActivity.kt
@@ -2,10 +2,35 @@ package com.example.gitstage
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.example.gitstage.databinding.ActivityMainBinding
+import com.example.gitstage.network.Callback2
+import com.example.gitstage.network.GitHubService
+
+//Adding binding, change in build.gradle
 
 class MainActivity : AppCompatActivity() {
+
+    val repoService = GitHubService()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.bt1.setOnClickListener {
+            repoService.fetchrepo(object: Callback2{
+                override fun repositoryResult(result: ArrayList<RepositoryResponseItem>) {
+                    Log.d("TEST","This is $result")
+                }
+            })
+
+        }
+
+
     }
+
+
+
 }
